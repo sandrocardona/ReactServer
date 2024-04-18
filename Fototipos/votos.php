b8058df86b77a611e2d54ba15dba8786efb8d3bd
sandrocardona <sandrocardona41@gmail.com>
2024-04-18 19:17:15 +0200

    18 Abril

--- Fototipos/votos.php
+++ Fototipos/votos.php
@@ -1,11 +1,11 @@
 <?php
-//casa
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization, token, Content-Type, cache-control");
 
 $_POST=json_decode(file_get_contents("php://input"),true);
-if(!isset($_POST["nachoFoto"])){
+if(!isset($_POST["idRespuesta"])){
     return;
 }
 
@@ -18,35 +18,35 @@ $uno = $array[0];
 $dos = $array[1];
 $tres = $array[2];
 $cuatro = $array[3];
-$cinco = $array[3];
-$seis = $array[3];
+$cinco = $array[4];
+$seis = $array[5];
 
 
 //extraemos el fototipo
-$fototipo = $_GET['voto'];
+$fototipo = $_POST['idRespuesta'];
 
 //actualizamos los fototipos añadiendo el recibido a los anteriores
 if ($fototipo == 0) {
     $uno += 1;
 }
 
-if ($fototipo == 0) {
+if ($fototipo == 1) {
     $dos += 1;
 }
 
-if ($fototipo == 0) {
+if ($fototipo == 2) {
     $tres += 1;
 }
 
-if ($fototipo == 0) {
+if ($fototipo == 3) {
     $cuatro += 1;
 }
 
-if ($fototipo == 0) {
+if ($fototipo == 4) {
     $cinco += 1;
 }
 
-if ($fototipo == 0) {
+if ($fototipo == 5) {
     $seis += 1;
 }
 
