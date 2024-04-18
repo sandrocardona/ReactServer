f31a3be45962a41ad2696ce784d626b9b37075e1
sandrocardona <sandrocardona41@gmail.com>
2024-04-18 17:44:59 +0200

    18 abril

--- Fototipos/votos.php
+++ Fototipos/votos.php
@@ -1,5 +1,5 @@
 <?php
-
+//casa
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization, token, Content-Type, cache-control");
