884418decb7fa4ca28eb60f75960d2a5006f0625
sandrocardona <sandrocardona41@gmail.com>
2024-04-23 12:42:17 +0200

    23 Abril

--- Applogin/conexion.php
+++ Applogin/conexion.php
@@ -1,9 +1,18 @@
 <?php
+
+        //variables constantes
+        define("SERVIDOR_BD","localhost");
+        define("USUARIO_BD","jose");
+        define("CLAVE_BD","josefa");
+        define("NOMBRE_BD","sc_usuarios");
+
         try{
             $conexion= new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8'"));
         }
         catch(PDOException $e){
             $respuesta["error"]="Imposible conectar a la BD en login()";
+            echo $respuesta["error"];
+            die();
         }
 
         try{
@@ -19,12 +28,10 @@
         if($sentencia->rowCount() > 0){
             /* recoger los datos */
             $respuesta["usuario"]=$sentencia->fetch(PDO::FETCH_ASSOC);
-            /* iniciar la sesion */
-            session_name("recuperacion22-23");
-            session_start();
+
             /* asignar valor a las variables */
-            $_SESSION["usuario"]=$respuesta["usuario"]["usuario"];
-            $_SESSION["clave"]=md5($respuesta["usuario"]["clave"]);
+            $usuario=$respuesta["usuario"]["usuario"];
+            $clave=md5($respuesta["usuario"]["clave"]);
             $respuesta["api_session"]=session_id();
         } else {
             $respuesta["mensaje"]="Usuario no registrado en la BD";
