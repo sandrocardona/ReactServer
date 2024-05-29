fb9246c2a6e085cbc17ac19ebb20d33fbbbf32e1
sandrocardona <sandrocardona41@gmail.com>
2024-05-29 18:03:50 +0200

    ax

---
+++ inmobiliaria/getContactos.php
@@ -0,0 +1,40 @@
+<?php
+    header('Access-Control-Allow-Origin: *');
+
+    define("SERVIDOR_BD","localhost");
+    define("USUARIO_BD","jose");
+    define("CLAVE_BD","josefa");
+    define("NOMBRE_BD","bd_inmobiliaria");
+
+    //GET DATA
+    try{
+        $conexion= new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES 'utf8'"));
+    }
+    catch(PDOException $e){
+        $respuesta["error"]="Imposible conectar a la BD en GET DATA";
+        echo $respuesta["error"];
+        die();
+    }
+
+    try{
+        $consulta="SELECT * FROM contacto";
+        $sentencia=$conexion->prepare($consulta);
+        $sentencia->execute();
+    }
+    catch(PDOException $e){
+        $respuesta["error"]="Imposible realizar la consulta a la BD en GET DATA";
+        $conexion=null;
+    }
+
+    if($sentencia->rowCount() > 0){
+        /* recoger los datos */
+        $respuesta["contactos"]=$sentencia->fetchAll(PDO::FETCH_ASSOC);
+    } else {
+        $respuesta["mensaje"]="No hay contactos en la BD";
+    }
+
+    $sentencia=null;
+    $conexion=null;
+
+    echo json_encode($respuesta);
+?>
